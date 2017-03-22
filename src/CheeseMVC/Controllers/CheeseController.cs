23d9ac5cd18a46beb5b37c7669963ee373170277
nicholascloud <me@nicholascloud.com>
2017-03-21 22:04:51 -0500

    class 8 prep code
    - intro to model binding

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -11,27 +11,16 @@ namespace CheeseMVC.Controllers
     // view location: /Views/Cheese/*
     public class CheeseController : Controller
     {
-        // static propery is available to *any* instance
-        // of the CheeseController class
-        private static List<Cheese> cheeses = new List<Cheese>();
-
-        // alternative dictionary implementation
-        //
-        // private static Dictionary<string, Cheese> cheeses = new Dictionary<string, Cheese>();
-
         // Display the list of cheeses
         // GET: /cheese
         public IActionResult Index()
         {
             // data for the view
-            ViewBag.cheeses = cheeses;
+            ViewBag.cheeses = CheeseData.GetAll();
 
             // view defaults to action name: Index.cshtml
             // /Views/Cheese/Index.cshtml
             return View();
-
-            // OR render particular view *by name*
-            // return View("Index");
         }
 
         public IActionResult Add()
@@ -45,29 +34,24 @@ namespace CheeseMVC.Controllers
         // by default, the route will be: /Cheese/NewCheese
         // but we want to post the form to the same route
         // from which we loaded it: /Cheese/Add
+        //
+        // Since the Cheese class has name and description
+        // attributes, we can let MVC create a new Cheese
+        // object for us, and fill its Name and Descirption
+        // properties automagically. Note that:
+        // - the Cheese class must have a *default* parameterless
+        //   constructor, and 
+        // - this does *not* yet perform any validation!
+        //
         [Route("/cheese/add")]
         [HttpPost]
-        public IActionResult NewCheese(string name = "", string description = "")
+        public IActionResult NewCheese(Cheese newCheese)
         {
             ViewBag.error = "";
-            
-            // valid cheese names: letters and spaces
-            if (!Cheese.IsValidName(name))
-            {
-                // cheese name is not valid, re-render the form and
-                // send an error message
-                ViewBag.error = "#fakecheese!";
-                return View("Add");
-            }
-   
+  
             // add new cheese to static class list
-            cheeses.Add(new Cheese(name, description));
-
-            // alternative dictionary implementation for
-            // Dictionary<string, Cheese>
-            //
-            // cheeses.Add(name, new Cheese(name, description));
-
+            CheeseData.Add(newCheese);
+            
             // go back to the list of cheeses
             return Redirect("/cheese");
         }
@@ -76,65 +60,31 @@ namespace CheeseMVC.Controllers
         [HttpGet]
         public IActionResult Remove()
         {
-            ViewBag.cheeses = cheeses;
+            ViewBag.cheeses = CheeseData.GetAll();
             return View("Remove");
         }
 
         [Route("/cheese/remove")]
         [HttpPost]
-        public IActionResult RemoveManyCheeses(string[] selectedCheeses)
+        public IActionResult RemoveManyCheeses(int[] cheeseIds)
         {
-            foreach (string cheeseName in selectedCheeses)
+            foreach (int submittedCheeseId in cheeseIds)
             {
-                // create an array so we aren't attempting to remove
-                // cheeses from the List<Cheese> while iterating
-                // over it at the same time
-                foreach (Cheese cheese in cheeses.ToArray())
-                {
-                    if (cheese.Name.Equals(cheeseName))
-                    {
-                        cheeses.Remove(cheese);
-                    }
-                }
+                // remove cheese from cheeses by using LINQ to extract
+                // the specific cheese by CheeseId
+                CheeseData.Remove(submittedCheeseId);
             }
-
-            // alternative dictionary implementation for
-            // Dictionary<string, Cheese>
-            //
-            // foreach (string cheeseName in selectedCheeses)
-            // {
-            //    if (cheeses.ContainsKey(cheeseName))
-            //    {
-            //        cheeses.Remove(cheeseName);
-            //   }
-            // }
 
             return Redirect("/cheese");
         }
 
-        [Route("/cheese/remove/{cheeseName}")]
+        [Route("/cheese/remove/{cheeseId}")]
         [HttpGet]
-        public IActionResult RemoveSingleCheese(string cheeseName = "")
+        public IActionResult RemoveSingleCheese(int cheeseId)
         {
-            // create an array so we aren't attempting to remove
-            // cheeses from the List<Cheese> while iterating
-            // over it at the same time
-            foreach (Cheese cheese in cheeses.ToArray())
-            {
-                if (cheese.Name.Equals(cheeseName))
-                {
-                    cheeses.Remove(cheese);
-                    break;
-                }
-            }
-
-            // alternative dictionary implementation for
-            // Dictionary<string, Cheese>
-            //
-            // if (cheeses.ContainsKey(cheeseName))
-            // {
-            //    cheeses.Remove(cheeseName);
-            // }
+            // remove cheese from cheeses by using LINQ to extract
+            // the specific cheese by CheeseId
+            CheeseData.Remove(cheeseId);
 
             return Redirect("/cheese");
         }
--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -8,15 +8,27 @@ namespace CheeseMVC.Models
 {
     public class Cheese
     {
+        public int CheeseId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
+        private static int nextId = 1;
+
         private static readonly Regex validCheeseNamePattern = new Regex(@"[a-zA-Z\s]+");
+
+        public Cheese()
+        {
+            // default constructor is required for model
+            // binding in controllers
+        }
 
         public Cheese(string name, string description)
         {
+            CheeseId = nextId;
             Name = name;
             Description = description;
+
+            nextId += 1;
         }
 
         public static bool IsValidName(string name)
